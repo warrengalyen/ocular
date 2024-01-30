3cdb3f852ec4e0d9be4efa96ad1ad5d289ee733d
warrengalyen <wgalyen@hotmail.com>
2024-01-30 13:10:05 -0800

    fix `ocularSaturationFilter`

--- lib/ocular.c
+++ lib/ocular.c
@@ -50,7 +50,7 @@ extern "C" {
     }; */
 
     static inline unsigned long byteswap_ulong(unsigned long i) {
-        unsigned int j;
+        unsigned long j;
         j = (i << 24);
         j += (i << 8) & 0x00FF0000;
         j += (i >> 8) & 0x0000FF00;
@@ -599,10 +599,10 @@ extern "C" {
         if (Channels == 1)
             return;
         unsigned char SaturationMap[256 * 256] = { 0 };
-        for (int grey = 0; grey < 256; grey++) {
-            unsigned char* pSaturationMap = SaturationMap + (grey << 8);
-            for (int Input = 0; Input < 256; Input++) {
-                pSaturationMap[0] = (unsigned char)((mix_u8((unsigned char)grey, (unsigned char)Input, saturation) + Input) * 0.5f);
+        for (int gray = 0; gray < 256; gray++) {
+            unsigned char* pSaturationMap = SaturationMap + (gray << 8);
+            for (int i = 0; i < 256; i++) {
+                pSaturationMap[0] = (unsigned char)((mix_u8((unsigned char)gray, (unsigned char)i, saturation) + i) * 0.5f);
                 pSaturationMap++;
             }
         }
