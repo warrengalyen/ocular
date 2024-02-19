b8764d42d69057a46e0dd7a417534ddc7b92d566
warrengalyen <wgalyen@hotmail.com>
2024-02-18 16:41:59 -0800

    add equalize filter

--- dll/ocular.h
+++ dll/ocular.h
@@ -133,6 +133,8 @@ extern "C" {
 
     DLIB_EXPORT void ocularAutoGammaCorrection(unsigned char* Input, unsigned char* Output, int Width, int Height, int Stride);
 
+    DLIB_EXPORT void ocularEqualizeFilter(unsigned char* Input, unsigned char* Output, int Width, int Height, int Stride);
+
     //--------------------------Color adjustments--------------------------
 
     //--------------------------Image processing--------------------------
