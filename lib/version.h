4b683915fdb2857f4faa357bcbd29c150fecc164
warrengalyen <wgalyen@hotmail.com>
2025-10-07 09:36:12 -0700

    update library version

--- lib/version.h
+++ lib/version.h
@@ -2,8 +2,8 @@
 #define OCULAR_VERSION_H
 
 #define VERSION_MAJOR "1"   
-#define VERSION_MINOR "1"
-#define VERSION_PATCH "2"
+#define VERSION_MINOR "2"
+#define VERSION_PATCH "0"
 #define VERSION_BUILD "0"
 
 #endif  /* OCULAR_VERSION_H */
